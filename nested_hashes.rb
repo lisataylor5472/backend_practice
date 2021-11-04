e337100c9049015bf099863733c0c412f7e315de
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-11-04 14:57:47 -0600

    Initial commit

--- nested_hashes.rb
+++ nested_hashes.rb
@@ -1,24 +1,59 @@
+# declaring a hash named states
 states = {
-  "Colorado" => { capital: "Denver", abbrevation: "CO" },
-  "Washington" => { capital: "Seattle", abbrevation: "WA" }
+  # creating a new hash with the key of Colorado with nested key value pairs of capital and abbreviation
+  "Colorado" => { cities: ["Denver", "Boulder"], abbreviation: "CO" },
+  # creating a new hash with the key of Washington with nested key value pairs of capital and abbreviation
+  "Washington" => { capital: "Seattle", abbreviation: "WA" }
 }
 
+# printing the value(s) of the Colorado key within the states hash
 p states["Colorado"]
-p states["Colorado"][:capital]
+# printing the value of the key capital within the Colorado states hash
+p states["Colorado"][:cities][0]
 
 
+# experimenting with accessing values within the hash
+p states["Colorado"][:cities][1]
+
+states["Colorado"][:cities][2] = "Fort Collins"
+p states["Colorado"][:cities]
+
+states["Colorado"][:cities] << "Colorado Springs"
+p states["Colorado"][:cities]
+
+states["Colorado"][:cities].push("Vail", "Steamboat Springs", "Pueblo")
+p states["Colorado"][:cities]
+
+
+
+
+# similar to above --- using different syntax
 states = {
-  "New York" => { "capital" => "Albany", "abbrevation" => "NY" },
-  "California" => { "capital" => "Sacramento", "abbrevation" => "CA" }
-}
+  "New York" => { "capital" => "Albany", "abbreviation" => "NY" },
+  "California" => { "capital" => "Sacramento", "abbreviation" => "CA" }
+  }
+
 
 p states["New York"]
 p states["California"]["capital"]
 
+# experimenting with methods
+states.each do |key,value|
+  puts "#{key} : #{value}"
+end
 
+# going further into accessing hashes and methods...
+states.each do |key,value|
+  value.each do |value,value2|
+  puts "#{key} : #{value} : #{value2}"
+  end
+end
+
+
+# similar to above --- using different syntax
 states = {
-  idaho: { capital: "Boise", abbrevation: "ID" },
-  oregon: { capital: "Salem", abbrevation: "OR" }
+  idaho: { capital: "Boise", abbreviation: "ID" },
+  oregon: { capital: "Salem", abbreviation: "OR" }
 }
 
 p states[:idaho]
